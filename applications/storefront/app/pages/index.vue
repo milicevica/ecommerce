e2eb957edf6901055c79273a261a9646fa530976
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-22 14:39:50 +0200

    async button component

--- applications/storefront/app/pages/index.vue
+++ applications/storefront/app/pages/index.vue
@@ -1,9 +1,19 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+async function expand() {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+}
+</script>
 
 <template>
   <div>
     <h1>Storefront</h1>
 
     <app-prompt-dialog title="Prompt Question" question="Are you sure you want to proceed?" />
+
+    <app-async-button :promise="expand">
+      Expand
+    </app-async-button>
   </div>
 </template>
