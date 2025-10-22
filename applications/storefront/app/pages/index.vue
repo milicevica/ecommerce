fcb8c7e7e066b4e9dbd807c17a4907ef9ab3db09
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-22 16:34:15 +0200

    add feature categories

--- applications/storefront/app/pages/index.vue
+++ applications/storefront/app/pages/index.vue
@@ -6,37 +6,98 @@ const items = [
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 w-full h-full">
-    <u-carousel
-      v-slot="{ item }"
-      :items="items"
-      autoplay
-      class="w-full h-full"
-    >
-      <div class="relative w-full h-full">
-        <img
-          :src="item.src"
-          :alt="item.alt"
-          class="w-full h-full object-cover"
-        >
+  <div class="flex flex-col gap-4">
+    <div class=" w-full h-full">
+      <u-carousel
+        v-slot="{ item }"
+        :items="items"
+        autoplay
+        class="w-full h-full"
+      >
+        <div class="relative w-full h-full">
+          <img
+            :src="item.src"
+            :alt="item.alt"
+            class="w-full h-full object-cover"
+          >
 
-        <div class="absolute inset-0 bg-black/40 flex flex-col gap-4 justify-center items-center text-center text-white p-6">
-          <h3 class="text-xl font-bold">
-            Fall Back In Style
-          </h3>
-          <h2 class="text-4xl font-bold">
-            Save 15% On Arabic & Mulberry Collections
-          </h2>
-          <div class="flex gap-4">
-            <u-button color="primary">
-              Explore Now
-            </u-button>
-            <u-button variant="ghost" color="neutral">
-              Clocks Go Back!
-            </u-button>
+          <div class="absolute inset-0 bg-black/40 flex flex-col gap-4 justify-center items-center text-center text-white p-6">
+            <h3 class="text-xl font-bold">
+              Fall Back In Style
+            </h3>
+            <h2 class="text-4xl font-bold">
+              Save 15% On Arabic & Mulberry Collections
+            </h2>
+            <div class="flex gap-4">
+              <u-button color="primary">
+                Explore Now
+              </u-button>
+              <u-button variant="ghost" color="neutral">
+                Clocks Go Back!
+              </u-button>
+            </div>
           </div>
         </div>
+      </u-carousel>
+    </div>
+
+    <div class="flex flex-col gap-4">
+      <div class="flex flex-col gap-4 p-12 text-center">
+        <h2 class="text-4xl font-bold">
+          Explore Our Clocks
+        </h2>
+        <p>Our designs take their cue from timeless interiors - crafted to become the standout piece that finds its perfect spot on yout shelf, sideboard, or wall.</p>
       </div>
-    </u-carousel>
+
+      <div class="flex gap-4">
+        <div class="flex flex-col gap-2">
+          <img
+            src="https://picsum.photos/640/640?random=1"
+            alt="Category 1"
+            width="320"
+            height="320"
+          >
+          <u-button variant="outline">
+            Living Room
+          </u-button>
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <img
+            src="https://picsum.photos/640/640?random=2"
+            alt="Category 2"
+            width="320"
+            height="320"
+          >
+          <u-button variant="outline">
+            Large Wall Clocks
+          </u-button>
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <img
+            src="https://picsum.photos/640/640?random=3"
+            alt="Category 3"
+            width="320"
+            height="320"
+          >
+          <u-button variant="outline">
+            Outdoors
+          </u-button>
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <img
+            src="https://picsum.photos/640/640?random=4"
+            alt="Category 4"
+            width="320"
+            height="320"
+          >
+          <u-button variant="outline">
+            Mantel Clocks
+          </u-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
