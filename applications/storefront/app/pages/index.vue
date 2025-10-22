2c2b017c27165f1c674fe3c7c968f62da89ed496
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-22 13:31:00 +0200

    add preconfigure modal called prompt-dialog, for simple yes/no questions

--- applications/storefront/app/pages/index.vue
+++ applications/storefront/app/pages/index.vue
@@ -1,32 +1,9 @@
-<script lang="ts" setup>
-import type { ModalElement } from "../../../../layers/ui/components/app/modal.vue";
-
-const modalEl = useTemplateRef<ModalElement>("modalEl");
-
-async function task() {
-  modalEl.value?.open();
-}
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div>
     <h1>Storefront</h1>
-    <u-button @click="task">
-      Open Modal
-    </u-button>
 
-    <AppModal ref="modalEl" description="Demo Modal">
-      <template #title>
-        <h2>Modal Title</h2>
-      </template>
-      <template #body>
-        <p>This is modal content</p>
-      </template>
-      <template #footer>
-        <u-button @click="modalEl?.close()">
-          Close
-        </u-button>
-      </template>
-    </AppModal>
+    <app-prompt-dialog title="Prompt Question" question="Are you sure you want to proceed?" />
   </div>
 </template>
