6380ef64e2af0dcf8f443ad812c294e494cc1c73
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-22 12:13:24 +0200

    demo modal component, wip

--- applications/storefront/app/pages/index.vue
+++ applications/storefront/app/pages/index.vue
@@ -1,3 +1,27 @@
+<script lang="ts" setup>
+import type { ModalElement } from "~/components/modal-demo.vue";
+
+const modalEl = useTemplateRef<ModalElement>("modalEl");
+
+async function task() {
+  modalEl.value?.open();
+}
+</script>
+
 <template>
-  <h1>Storefront</h1>
+  <div>
+    <h1>Storefront</h1>
+    <button @click="task">
+      Open Modal
+    </button>
+
+    <ModalDemo ref="modalEl">
+      <template #title>
+        <h2>Modal Title</h2>
+      </template>
+      <template #body>
+        <p>This is modal content</p>
+      </template>
+    </ModalDemo>
+  </div>
 </template>
