51f35bafcc51bb1e6a62ad5aa27db787cd5a2d18
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-23 11:30:42 +0200

    fix gap in message component

--- applications/storefront/app/components/app/message.vue
+++ applications/storefront/app/components/app/message.vue
@@ -5,7 +5,9 @@
     <template #title>
       <div class="flex items-center justify-center gap-2">
         <u-icon name="tabler:truck-delivery" size="24" />
-        FREE Delivery for orders <span class="font-bold">over $100</span>
+        <p>
+          FREE Delivery for orders <span class="font-bold">over $100</span>
+        </p>
       </div>
     </template>
   </u-banner>
