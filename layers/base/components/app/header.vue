58657c122ae1489f7a9b4c2bfe728dc3e6c8da3a
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-27 12:46:35 +0100

    move header component to base layer

--- layers/base/components/app/header.vue
+++ layers/base/components/app/header.vue
@@ -42,33 +42,8 @@ const actionMenu = computed<NavigationMenuItem[]>(() => [
 
 <template>
   <div>
-    <u-header class="border-b-0">
-      <template #title>
-        ECOMMERCE
-      </template>
+    <header-toolbar />
 
-      <template #toggle>
-        <span />
-      </template>
-
-      <template #right>
-        <u-locale-select
-          v-model="locale"
-          :locales="[en, fr]"
-          @update:model-value="$event === 'en' || $event === 'fr' ? setLocale($event) : setLocale('en')"
-        />
-        <app-theme-toggle />
-
-        <u-tooltip text="Log In">
-          <u-button
-            color="neutral"
-            variant="ghost"
-            icon="tabler:user"
-            aria-label="Log In"
-          />
-        </u-tooltip>
-      </template>
-    </u-header>
     <u-header title="">
       <template #title>
         <u-dropdown-menu
---
+++ layers/base/components/header/toolbar.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { en, fr } from "@nuxt/ui/locale";
+
+const { locale, setLocale } = useI18n();
+</script>
+
+<template>
+  <u-header class="border-b-0">
+    <template #title>
+      ECOMMERCE
+    </template>
+
+    <template #toggle>
+      <span />
+    </template>
+
+    <template #right>
+      <u-locale-select
+        v-model="locale"
+        :locales="[en, fr]"
+        @update:model-value="$event === 'en' || $event == 'fr' ? setLocale($event) : setLocale('en')"
+      />
+
+      <app-theme-toggle />
+
+      <u-tooltip text="Log In">
+        <u-button
+          color="neutral"
+          variant="ghost"
+          icon="tabler:user"
+          aria-label="Log In"
+        />
+      </u-tooltip>
+    </template>
+  </u-header>
+</template>
