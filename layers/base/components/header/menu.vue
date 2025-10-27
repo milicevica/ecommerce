c2f86d42e683ffd85b0477803ef354621bd2e1aa
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-27 16:57:30 +0100

    format menu component

--- layers/base/components/header/menu.vue
+++ layers/base/components/header/menu.vue
@@ -29,82 +29,59 @@ const items = ref([
 </script>
 
 <template>
-      <u-header title="" class="bg-accented border-b-0">
-      <template #left>
-        <u-navigation-menu :items="items" content-orientation="vertical" class="w-full" />
-      </template>
+  <u-header title="" class="bg-accented border-b-0">
+    <template #left>
+      <u-navigation-menu :items="items" content-orientation="vertical" class="w-full" />
+    </template>
 
-      <template #body>
-        <u-navigation-menu :items="actionMenu">
+    <template #body>
+      <u-navigation-menu :items="actionMenu">
+        <template #item="{ item }">
+          <u-tooltip :text="item.label">
+            <u-button color="neutral" variant="ghost" to="/" :icon="item.icon" :aria-label="item.label" />
+          </u-tooltip>
+        </template>
+      </u-navigation-menu>
+    </template>
+
+    <template #right>
+      <client-only>
+        <u-navigation-menu :items="actionMenu" class="hidden lg:flex">
           <template #item="{ item }">
-            <u-tooltip :text="item.label">
-              <u-button
-                color="neutral"
-                variant="ghost"
-                to="/"
-                :icon="item.icon"
-                :aria-label="item.label"
-              />
-            </u-tooltip>
+            <template v-if="item.label === 'Cart'">
+              <u-slideover title="My Cart">
+                <u-tooltip :text="$t('cart')">
+                  <u-button color="neutral" variant="ghost" to="/" :icon="item.icon" :aria-label="item.label" />
+                </u-tooltip>
+
+                <template #body>
+                  <div class="h-full flex items-center justify-center font-bold uppercase">
+                    {{ $t("cartEmpty") }}
+                  </div>
+                </template>
+              </u-slideover>
+            </template>
+            <template v-else-if="item.label === 'Search'">
+              <u-slideover side="top" title="Search">
+                <u-tooltip :text="item.label">
+                  <u-button color="neutral" variant="ghost" :icon="item.icon" :aria-label="item.label" />
+                </u-tooltip>
+
+                <template #body>
+                  <div class="flex w-full p-6">
+                    <u-input placeholder="Search For" class="w-full" />
+                  </div>
+                </template>
+              </u-slideover>
+            </template>
+            <template v-else>
+              <u-tooltip :text="item.label">
+                <u-button color="neutral" variant="ghost" to="/" :icon="item.icon" :aria-label="item.label" />
+              </u-tooltip>
+            </template>
           </template>
         </u-navigation-menu>
-      </template>
-
-      <template #right>
-        <client-only>
-          <u-navigation-menu :items="actionMenu" class="hidden lg:flex">
-            <template #item="{ item }">
-              <template v-if="item.label === 'Cart'">
-                <u-slideover title="My Cart">
-                  <u-tooltip :text="$t('cart')">
-                    <u-button
-                      color="neutral"
-                      variant="ghost"
-                      to="/"
-                      :icon="item.icon"
-                      :aria-label="item.label"
-                    />
-                  </u-tooltip>
-
-                  <template #body>
-                    <div class="h-full flex items-center justify-center font-bold uppercase">
-                      {{ $t("cartEmpty") }}
-                    </div>
-                  </template>
-                </u-slideover>
-              </template>
-              <template v-else-if="item.label === 'Search'">
-                <u-slideover side="top" title="Search">
-                  <u-tooltip :text="item.label">
-                    <u-button
-                      color="neutral"
-                      variant="ghost"
-                      :icon="item.icon"
-                      :aria-label="item.label"
-                    />
-                  </u-tooltip>
-
-                  <template #body>
-                    <div class="flex w-full p-6">
-                      <u-input placeholder="Search For" class="w-full" />
-                    </div>
-                  </template>
-                </u-slideover>
-              </template>
-              <template v-else>
-                <u-tooltip :text="item.label">
-                  <u-button
-                    color="neutral"
-                    variant="ghost"
-                    to="/"
-                    :icon="item.icon"
-                    :aria-label="item.label"
-                  />
-                </u-tooltip>
-              </template>
-            </template>
-          </u-navigation-menu>
-        </client-only>
-      </template>
-    </u-header>
+      </client-only>
+    </template>
+  </u-header>
 </template>
