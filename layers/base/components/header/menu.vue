92182bd2f579c08beb522bb107445628ffb7a538
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-28 12:32:55 +0100

    work on search

--- layers/base/components/header/menu.vue
+++ layers/base/components/header/menu.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import type { NavigationMenuItem } from "@nuxt/ui";
+import type { NavigationMenuItem, TabsItem } from "@nuxt/ui";
 import type { Product } from "../../shared/types/product";
 
 const searchTerm = ref("");
@@ -30,6 +30,22 @@ async function search() {
 
   foundProducts.value = data || [];
 }
+
+
+const tabs = ref<TabsItem[]>([
+  {
+    label: "Products",
+    slot: "products",
+  },
+  {
+    label: "Categories",
+    slot: "categories",
+  },
+  {
+    label: "Orders",
+    slot: "orders",
+  }
+]);
 </script>
 
 <template>
@@ -72,14 +88,35 @@ async function search() {
                 </u-tooltip>
 
                 <template #body>
-                  <div class="flex flex-col w-full p-6 gap-12">
+                  <div class="flex flex-col w-full p-6 gap-8">
                     <u-input v-model="searchTerm" placeholder="Search For" class="w-full" @update:model-value="search" />
 
-                    <div v-if="foundProducts.length">
-                      <template v-for="product in foundProducts" :key="product.id">
-                        <p>{{ product.name }}</p>
-                      </template>
-                    </div>
+                    <template v-if="searchTerm.length">
+                      <div class="flex gap-8 w-full items-start">
+                        <div class="flex w-full">
+                          <u-tabs :items="tabs" :unmount-on-hide="false" size="xl" variant="link" class="w-full mx-autol">
+                            <template #products>
+                              <div v-if="foundProducts.length">
+                                <template v-for="product in foundProducts" :key="product.id">
+                                  <p>{{ product.name }}</p>
+                                </template>
+                              </div>
+                              <div v-else>
+                                <p>No results could be found. Please try again with a different query.</p>
+                              </div>
+                            </template>
+
+                            <template #categories>
+                              <p>No results could be found. Please try again with a different query.</p>
+                            </template>
+
+                            <template #orders>
+                              <p>No results could be found. Please try again with a different query.</p>
+                            </template>
+                          </u-tabs>
+                        </div>
+                      </div>
+                    </template>
                   </div>
                 </template>
               </u-slideover>
