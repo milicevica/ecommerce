fcb532c2c70b1a19bb9cdc6ca662a86ab00f925d
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-29 11:36:15 +0100

    split demo-api into separate json files

---
+++ applications/demo-api/db.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+
+module.exports = () => {
+  const data = {
+    categories: JSON.parse(fs.readFileSync("./data/categories.json")),
+    products: JSON.parse(fs.readFileSync("./data/products.json")),
+  }
+
+  return data
+}
