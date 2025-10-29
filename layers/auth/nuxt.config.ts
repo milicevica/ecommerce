12645786bd59566b8e324d8f9ec5d35a4a6e121e
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-29 14:58:56 +0100

    install pinia nuxt module inside auth layer

--- layers/auth/nuxt.config.ts
+++ layers/auth/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ["../ui", "../i18n"]
+  extends: ["../ui", "../i18n"],
+  modules: ["@pinia/nuxt"]
 })
