3381e950831c043e540eb4b6da8cf059225a71ac
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-29 13:53:24 +0100

    whishlist icon component, and hide menu toggle

--- layers/base/components/header/menu.vue
+++ layers/base/components/header/menu.vue
@@ -1,43 +1,18 @@
-<script lang="ts" setup>
-import type { NavigationMenuItem } from "@nuxt/ui";
-
-const actionMenu = computed<NavigationMenuItem[]>(() => [
-  {
-    label: "Wishlist",
-    icon: "tabler:heart",
-  },
-]);
-</script>
-
 <template>
   <u-header title="" class="bg-accented border-b-0">
     <template #left>
       <header-store-menu />
     </template>
 
-    <template #body>
-      <u-navigation-menu :items="actionMenu">
-        <template #item="{ item }">
-          <u-tooltip :text="item.label">
-            <u-button color="neutral" variant="ghost" to="/" :icon="item.icon" :aria-label="item.label" />
-          </u-tooltip>
-        </template>
-      </u-navigation-menu>
+    <template #toggle>
+      <span />
     </template>
 
     <template #right>
       <client-only>
         <search-btn />
+        <whishlist-icon />
         <cart-icon />
-        <u-navigation-menu :items="actionMenu" class="hidden lg:flex">
-          <template #item="{ item }">
-
-              <u-tooltip :text="item.label">
-                <u-button color="neutral" variant="ghost" to="/" :icon="item.icon" :aria-label="item.label" />
-              </u-tooltip>
-
-          </template>
-        </u-navigation-menu>
       </client-only>
     </template>
   </u-header>
---
+++ layers/base/components/whishlist/icon.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <u-tooltip text="Wishlist">
+    <u-button color="neutral" variant="ghost" to="/" icon="tabler:heart" aria-label="Wishlist" />
+  </u-tooltip>
+</template>
