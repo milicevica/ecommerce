98843ed6af5a6d85c50d3f7772757cb2be97b507
Aleksandar Milicevic <hi@milicevic.dev>
2025-10-29 13:49:55 +0100

    cart icon

---
+++ layers/base/components/cart/icon.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { useCartStore } from '../../stores/use-cart-store';
+
+const store = useCartStore();
+</script>
+
+<template>
+  <u-slideover title="My Cart">
+    <u-tooltip :text="$t('cart')">
+      <u-chip
+        color="info"
+        size="3xl"
+        :show="store.numberOfItemsInCart > 0"
+        :text="store.numberOfItemsInCart"
+        inset
+      >
+        <u-button color="neutral" variant="ghost" to="/" icon="tabler:shopping-bag" :aria-label="$t('cart')" />
+      </u-chip>
+    </u-tooltip>
+  </u-slideover>
+</template>
--- layers/base/components/header/menu.vue
+++ layers/base/components/header/menu.vue
@@ -1,17 +1,10 @@
 <script lang="ts" setup>
 import type { NavigationMenuItem } from "@nuxt/ui";
-import { useCartStore } from "../../stores/use-cart-store";
-
-const cartStore = useCartStore();
 
 const actionMenu = computed<NavigationMenuItem[]>(() => [
   {
     label: "Wishlist",
     icon: "tabler:heart",
-  },
-  {
-    label: "Cart",
-    icon: "tabler:shopping-bag",
   },
 ]);
 </script>
@@ -35,28 +28,14 @@ const actionMenu = computed<NavigationMenuItem[]>(() => [
     <template #right>
       <client-only>
         <search-btn />
+        <cart-icon />
         <u-navigation-menu :items="actionMenu" class="hidden lg:flex">
           <template #item="{ item }">
-            <template v-if="item.label === 'Cart'">
-              <u-slideover title="My Cart">
-                <u-tooltip :text="$t('cart')">
-                  <u-chip color="info" :show="cartStore.numberOfItemsInCart > 0" :text="cartStore.numberOfItemsInCart" size="3xl" inset>
-                    <u-button color="neutral" variant="ghost" to="/" :icon="item.icon" :aria-label="item.label" />
-                  </u-chip>
-                </u-tooltip>
 
-                <template #body>
-                  <div class="h-full flex items-center justify-center font-bold uppercase">
-                    {{ $t("cartEmpty") }}
-                  </div>
-                </template>
-              </u-slideover>
-            </template>
-            <template v-else>
               <u-tooltip :text="item.label">
                 <u-button color="neutral" variant="ghost" to="/" :icon="item.icon" :aria-label="item.label" />
               </u-tooltip>
-            </template>
+
           </template>
         </u-navigation-menu>
       </client-only>
