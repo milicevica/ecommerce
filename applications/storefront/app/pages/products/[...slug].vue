0a8c5c3d9eecbcbc9e806eb0f0a3e8e1e9348e8d
Aleksandar Milicevic <hi@milicevic.dev>
2025-11-07 12:14:46 +0100

    remove unusude pages from storefront app

--- applications/storefront/app/pages/products/[...slug].vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-const route = useRoute();
-
-const slug = route.params.slug ? route.params.slug[0] : "";
-</script>
-
-<template>
-  <p>Products: {{ slug }}</p>
-</template>
--- applications/storefront/app/pages/sale.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <p>Sale</p>
-</template>
